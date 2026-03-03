b24865a6d7059c1edabf4fae359ef12785e27e4d
okikeSolutions <ugo.uwakwe@okike-solutions.com>
2026-03-03 22:34:54 +0100

    chore: fix lint errors

--- apps/web/convex/betterAuth/auth.ts
+++ apps/web/convex/betterAuth/auth.ts
@@ -1,4 +1,5 @@
 import { createAuth } from "../auth";
 
 // Export a static instance for Better Auth schema generation
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const auth = createAuth({} as any);
